78d086d3900067e82a43894307baf73872154ba2
Kamil Zieliński <kamil.zielinski@miquido.com>
2020-05-11 12:26:55 +0200

    Updates

---
+++ precache-manifest.24d9cc160a45fc288c6e78410f62d781.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "439abe9c8fc3dd8e594959bebfb32e4d",
+    "url": "/sr-video-liker/index.html"
+  },
+  {
+    "revision": "254d41d2d051846d438e",
+    "url": "/sr-video-liker/static/css/2.537602a3.chunk.css"
+  },
+  {
+    "revision": "19596fb2b2eca990ae02",
+    "url": "/sr-video-liker/static/css/main.fe1861b9.chunk.css"
+  },
+  {
+    "revision": "254d41d2d051846d438e",
+    "url": "/sr-video-liker/static/js/2.b9d42027.chunk.js"
+  },
+  {
+    "revision": "af0a063b7aacc833132980c2224e7d2e",
+    "url": "/sr-video-liker/static/js/2.b9d42027.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19596fb2b2eca990ae02",
+    "url": "/sr-video-liker/static/js/main.47b68a72.chunk.js"
+  },
+  {
+    "revision": "48a11273fdf4cf6078af",
+    "url": "/sr-video-liker/static/js/runtime-main.a63468ec.js"
+  }
+]);
